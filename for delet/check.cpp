3616c9c06edf37473ac1ac3e5a0d95355d71b45d
tamim1715 <60297470+tamim1715@users.noreply.github.com>
2020-11-24 01:11:55 +0600

    Add files via upload

--- for delet/check.cpp
+++ for delet/check.cpp
@@ -1,123 +1,78 @@
-
-/* SHAHADATH HOSSAIN TAMIM
-   BANGLADESH UNIVERSITY OF BUSINESS AND TECHNOLOGY
-*/
-
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
+#define LL long long
+#define SZ(x) ((int)(x).size())
+#define ALL(x) (x).begin(),(x).end()
+#define REP(i,n) for(int i=0;i<n;i++)
+#define FOR(i,a,b) for(int i=a;i<=b;i++)
+#define REV(i,n) for(int i=n-1;i>=0;i--)
+#define pri(a) cout << a << endl;
+#define prii(a,b) cout << a << " " << b << endl;
 using namespace std;
 
-#define ll long long
+#define sz 100007
+int a[sz], ltfrq[sz], rtfrq[sz], tr[4*sz];
 
-#define pb push_back
-#define pob pop_back
-
-#define gcd __gcd
-#define lcm(a,b) (a*b)/__gcd(a,b)
-
-#define L length()
-
-
-#define all(a) a.begin(), a.end()
-#define CLR(a)  memset(a,0,sizeof(a))
-
-#define pii pair<int, int>
-#define F  first
-#define S  second
-
-#define f(a,b) for(int i=a; i<b; i++)
-#define fl(a,b) for(long long i=a; i<b; i++)
-
-#define yes cout<<"YES"<<endl;
-#define no cout<<"NO"<<endl;
-
-#define INF 1e9
-#define INFL 1e18
-#define mod 1000000007
-
-#define pi 3.14159265358979323846
-
-
-int char_to_int(char c){int x=c-48; return x;}
-char int_to_char(int x){int c=x+48; return c;}
-
-
-void fast() {ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
-
-map<int, int>mp;
-vector<int>v;
-map<int, vector<int> >G;
-map<int, int>visited;
-
-int dx[] = {-1, 0, 1, 0};
-int dy[] = {0, 1, 0, -1};
-int cnt=0;
-int t[200009],ans[200005],check[200005],k;
-
-struct tamim
+void build(int node, int b, int e)
 {
-    int index,l,r;
-
-}query[200005];
-
-bool cmp(tamim a, tamim b)
-{
-    int own_index=a.l/k;
-    int other_index=b.l/k;
-    if(own_index==other_index) return a.r<b.r;
-    return own_index<other_index;
+    if(b==e)
+    {
+        tr[node] = ltfrq[b];
+        return;
+    }
+    int mid = (b+e)>>1;
+    int lft = node<<1, rgt = lft+1;
+    build(lft,b,mid);
+    build(rgt,mid+1,e);
+    tr[node] = max(tr[lft], tr[rgt]);
 }
-void add(int cur)
+
+int query(int node, int b, int e, int i, int j)
 {
-    check[t[cur]]++;
-    if(check[t[cur]]==1) cnt++;
-}
-void remove(int cur)
-{
-    check[t[cur]]--;
-    if(check[t[cur]]==0) cnt--;
+    if(b>=i and e<=j) return tr[node];
+
+    int mid = (b+e)>>1;
+    int lft = node<<1, rgt = lft+1;
+
+    if(j<=mid) return query(lft,b,mid,i,j);
+    else if(i>mid) return query(rgt,mid+1,e,i,j);
+    else return max(query(lft,b,mid,i,j), query(rgt,mid+1,e,i,j));
 }
 
 int main()
 {
-    //freopen("tamim.txt", "w", stdout);
-    fast();
-    int tst,cas=0;
-    scanf("%d", &tst);
-    while(tst--)
+    int n,q,l,r,res;
+    while(scanf("%d", &n)==1 and n!=0)
     {
-        CLR(check);
-        //mp.clear();
-        cnt=0;
-        int n,q;
-        scanf("%d %d", &n, &q);
-        k=n/sqrt(q*2/3);
-        for(int i=1; i<=n; i++) scanf("%d", &t[i]);
-        for(int i=1; i<=q; i++)
-        {
-            scanf("%d %d", &query[i].l, &query[i].r);
-            query[i].index=i;
+        scanf("%d",&q);
+        REP(i,n) scanf("%d",&a[i+1]);
 
-        }
-        sort(query+1, query+q+1, cmp);
-        int l=1,r=0;
-        for(int i=1; i<=q; i++)
+        ltfrq[1] = 1;
+        FOR(i,2,n)
         {
-            while(r<query[i].r) add(++r);
-            while(l<query[i].l) remove(l++);
-            while(r>query[i].r) remove(r--);
-            while(l>query[i].l) add(--l);
-            ans[query[i].index]=cnt;
+            if(a[i]==a[i-1]) ltfrq[i] = ltfrq[i-1] + 1;
+            else ltfrq[i] = 1;
         }
-        printf("Case %d:\n", ++cas);
-        //cout<<"Case "<<++cas<<":"<<endl;
-        for(int i=1; i<=q; i++)
+
+        rtfrq[n] = n;
+        for(int i=n-1; i>=1; i--)
         {
-            printf("%d\n", ans[i]);
+            if(a[i]==a[i+1]) rtfrq[i] = rtfrq[i+1] + 1;
+            else rtfrq[i] = 1;
+        }
+
+        build(1,1,n);
+
+        REP(i,q)
+        {
+            scanf("%d%d",&l,&r);
+
+            if(a[l]==a[r]) res = r-l+1;
+            else
+            {
+                res = max(rtfrq[l],query(1,1,n,l+rtfrq[l],r));
+            }
+            printf("%d\n", res);
         }
     }
-
-
     return 0;
 }
-
